632e14818d74e77b24c82a9b310453403147fec7
ubuntu <ubuntu@localhost.localdomain>
2024-10-28 18:36:03 +0800

    fork comic-text-detector

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ from jinja2 import Environment, FileSystemLoader
 import subprocess
 import json
 import time
+import sys
+sys.path.append('detector')
+from detector import inference, init
+
 app = Flask(__name__)
 app.config['OUTPUT_FOLDER'] = 'output'
 env = Environment(loader=FileSystemLoader('templates'))
@@ -18,11 +22,10 @@ web_debug = False #仅调试网页，不加载模型
 def parse_img(folder_name: str):
     if folder_name.index('.') != -1:
         folder_name = folder_name.split('.')[0]
-    # todo: 这样每次解析都要重新加载模型，实在是下下策
-    command = 'cd comic-text-detector && python inference.py'
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
-    picture_num = int(result.stdout.replace('\n',''))
-    with open('./output/output.txt', 'w', encoding='utf-8') as f:
+
+    picture_num = inference(detector_model)
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    with open(f'{current_directory}/output/output.txt', 'w', encoding='utf-8') as f:
         for i in range(picture_num):
             image_path = f'./output/cut_image_{i}.png'
             if not os.path.exists(image_path):
@@ -32,9 +35,11 @@ def parse_img(folder_name: str):
                 continue
             f.write(infer_text+'\n')
             f.write('请分析此日语句子的语法并为汉字标注假名：' + infer_text + '\n')
-    with open('./store/saved.json', 'a+', encoding='utf-8') as f:
-        source_folder = './output'
-        destination_folder = f'./store/{folder_name}'
+    with open(f'{current_directory}/store/saved.json', 'r+', encoding='utf-8') as f:
+        # 将文件指针移到文件开头
+        f.seek(0)
+        source_folder = f'{current_directory}/output'
+        destination_folder = f'{current_directory}/store/{folder_name}'
         os.makedirs(destination_folder)
         shutil.copytree(source_folder, destination_folder, dirs_exist_ok=True)
         data = json.load(f)
@@ -47,6 +52,12 @@ def parse_img(folder_name: str):
             data['files'].append(info)
         else:
             data['files'] = [info]
+        # 将文件指针移到文件开头，准备覆盖原有数据
+        f.seek(0)
+        # 清空文件内容
+        f.truncate()
+        
+        # 写入更新后的数据
         json.dump(data, f, ensure_ascii=False, indent=4)
         
 @app.route('/upload', methods=['POST'])
@@ -56,7 +67,7 @@ def upload_file():
     if 'file' not in request.files:
         return '没有文件部分', 400
     file = request.files['file']
-    # 如果用户没有选择文件，浏览器也会提交一个没有文件名的空部分
+
     if file.filename == '':
         return '没有选择文件', 400
     if file:
@@ -103,5 +114,6 @@ def uploaded_file(filename):
 #     return text, 200
 if not web_debug:
     mocr = MangaOcr()
+    detector_model = init()
 CORS(app)
 app.run(host='0.0.0.0', debug=True)
